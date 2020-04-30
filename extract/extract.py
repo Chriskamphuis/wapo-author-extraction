4d90e3aa7e30de83b80c58f3064bddd3179bd732
chriskamphuis <mail@chriskamphuis.com>
2020-04-30 17:04:53 +0200

    last changes

---
+++ extract/extract.py
@@ -0,0 +1,7 @@
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--collection', required=True, help='Path of collection file')
+
+    print("Hello world")
